34f0a39b6aced8177965b0c473bf0f083effb8c4
Callum McKenna <contact@callum.id.au>
2020-06-17 21:28:37 +1000

    Drawing a grid

--- src/main.c
+++ src/main.c
@@ -100,6 +100,16 @@ void process_input(void) {
 void update(void) {
 }
 
+void draw_grid(void) {
+    for (int y = 0; y < window_height; y++) {
+        for (int x = 0; x < window_width; x++) {
+            if (y % 10 == 0 || x % 10 == 0) {
+                color_buffer[window_width * y + x] = 0xFFFFFFFF;
+            }
+        }
+    }
+}
+
 void clear_color_buffer(uint32_t color) {
     for (int y = 0; y < window_height; y++) {
         for (int x = 0; x < window_width; x++) {
@@ -123,8 +133,10 @@ void render(void) {
     SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
     SDL_RenderClear(renderer);
 
+    draw_grid();
+
     render_color_buffer();
-    clear_color_buffer(0xFFFFFF00);
+    clear_color_buffer(0xFF000000);
 
     SDL_RenderPresent(renderer);
 }
