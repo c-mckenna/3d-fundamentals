8be9df685842d299eb4dd1c24bd31e8b558084d9
Callum McKenna <contact@callum.id.au>
2020-06-19 22:25:48 +1000

    Perspective projection

--- src/main.c
+++ src/main.c
@@ -10,7 +10,9 @@
 vec3_t cube_points[N_POINTS];
 vec2_t projected_points[N_POINTS];
 
-float fov_factor = 128;
+vec3_t camera_position = { .x = 0, .y = 0,  .z = -5 };
+
+float fov_factor = 640;
 
 bool is_running = false;
 
@@ -58,8 +60,8 @@ void process_input(void) {
 
 vec2_t project(vec3_t point) {
     vec2_t projected_point = {
-        .x = (fov_factor * point.x),
-        .y = (fov_factor * point.y)
+        .x = (fov_factor * point.x) / point.z,
+        .y = (fov_factor * point.y) / point.z
     };
     return projected_point;
 }
@@ -67,6 +69,9 @@ vec2_t project(vec3_t point) {
 void update(void) {
     for (int i = 0; i < N_POINTS; i++) {
         vec3_t point = cube_points[i];
+
+        // Move the point away from the camera
+        point.z -= camera_position.z;
 
         // Project the current point
         vec2_t projected_point = project(point);
