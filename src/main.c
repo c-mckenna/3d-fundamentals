a16abd1fd4b0adf86485d8e789af56a6920a254e
Callum McKenna <contact@callum.id.au>
2020-06-20 10:00:58 +1000

    Rotating vectors

--- src/main.c
+++ src/main.c
@@ -11,6 +11,7 @@ vec3_t cube_points[N_POINTS];
 vec2_t projected_points[N_POINTS];
 
 vec3_t camera_position = { .x = 0, .y = 0,  .z = -5 };
+vec3_t cube_rotation = { .x = 0, .y = 0, .z = 0 };
 
 float fov_factor = 640;
 
@@ -67,14 +68,22 @@ vec2_t project(vec3_t point) {
 }
 
 void update(void) {
+    cube_rotation.x += 0.01;
+    cube_rotation.y += 0.01;
+    cube_rotation.z += 0.01;
+
     for (int i = 0; i < N_POINTS; i++) {
         vec3_t point = cube_points[i];
 
-        // Move the point away from the camera
-        point.z -= camera_position.z;
+        vec3_t transformed_point = vec3_rotate_x(point, cube_rotation.x);
+        transformed_point = vec3_rotate_y(transformed_point, cube_rotation.y);
+        transformed_point = vec3_rotate_z(transformed_point, cube_rotation.z);
+
+        // Translated the point away from the camera
+        transformed_point.z -= camera_position.z;
 
         // Project the current point
-        vec2_t projected_point = project(point);
+        vec2_t projected_point = project(transformed_point);
 
         // Save the projected 2D vector in the array of projected points
         projected_points[i] = projected_point;
